8e55db565308634cace94f8080203859a662f477
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-08-06 20:08:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         fontSize: 40,
-        color: '#010101'
+        color: "#010101",
       }}
     >
       React homework template
